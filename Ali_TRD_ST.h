45c0acbd4801717d73321d41562fb0b20a1a7eab
Max Hammermann <hammermann.maximilian@gmail.com>
2022-05-03 14:55:49 +0200

    added TPC-GNN_track_class

--- Ali_TRD_ST.h
+++ Ali_TRD_ST.h
@@ -54,7 +54,197 @@ public:
 };
 //----------------------------------------------------------------------------------------
 
+class Ali_TRD_ST_TPC_Track_GNN : public TObject
+{
+private:
+    // Track properties
+    //Float_t        nsigma_e_TPC; // nsigma dE/dx of particle
 
+    //Float_t        nsigma_e_TOF; // nsigma TOF of particle
+    //Float_t        nsigma_pi_TPC; // nsigma dE/dx of particle
+    //Float_t        nsigma_pi_TOF; // nsigma TOF of particle
+    //Float_t        nsigma_K_TPC; // nsigma dE/dx of particle
+    //Float_t        nsigma_K_TOF; // nsigma TOF of particle
+    //Float_t        nsigma_p_TPC; // nsigma dE/dx of particle
+    //Float_t        nsigma_p_TOF; // nsigma TOF of particle
+    //Float_t        TRD_signal; // TRD PID signal
+    //Float_t        TRDsumADC; // TRD electron PID probability
+    //Float_t        dca; // distance of closest approach of particle A
+    TLorentzVector TLV_part; // Lorentz vector properties of this particle
+    //UShort_t       NTPCcls; // Number of TPC clusters
+    //UShort_t       NTRDcls; // Number of TRD clusters
+    //UShort_t       NITScls; // Number of TRD clusters
+    //UShort_t       status; // status of track: bit 0: ITS refit, bit1: TPC refit
+    //Float_t        TPCchi2; // TPC chi2
+    //Float_t        impact_angle_on_TRD; // Track impact angle on TRD
+    Float_t        TPCdEdx; // Energy loss information of TPC
+    //Float_t        TOFsignal; // Time-of-flight
+    //Float_t        Track_length; // length of track
+    Int_t         sector_of_incidence; // sector of incidence
+    Int_t         event_no;
+    Int_t         track_number;
+
+
+    Int_t          fNumTracklets; //number of Tracklets around given TPC_track   
+    TClonesArray*  fTracklets; 
+
+    Float_t        aliHelix_params[6];
+    Float_t        aliHelix_TRD_params[6];    
+
+    // physics information
+    Float_t momentum;
+    Float_t energy;
+    Float_t eta_track;
+    Float_t pT_track;
+    Float_t theta_track;
+    Float_t phi_track;
+public:
+    Ali_TRD_ST_TPC_Track_GNN() :
+        //nsigma_e_TPC(-3),nsigma_e_TOF(-3),nsigma_pi_TPC(-3),nsigma_pi_TOF(-3),nsigma_K_TPC(-3),nsigma_K_TOF(-3),nsigma_p_TPC(-3),nsigma_p_TOF(-3),TRD_signal(-3),
+        TLV_part(), fNumTracklets(0), fTracklets(), TPCdEdx(-3), aliHelix_params(), momentum(), eta_track(), energy(),
+        pT_track(), theta_track(), phi_track(), sector_of_incidence()
+        //TRDsumADC(-3),dca(-3),NTPCcls(-3),NTRDcls(-3),NITScls(-3),status(-3),TPCchi2(-3), aliHelix_TRD_params(), impact_angle_on_TRD(-3), TOFsignal(-3),
+        //Track_length(-3), 
+    {
+        fTracklets                = new TClonesArray( "Ali_TRD_ST_Tracklets_for_GNN", 10 );
+    }
+	~Ali_TRD_ST_TPC_Track_GNN()
+	{
+        delete fTracklets;
+        fTracklets = NULL;
+	}
+
+	// setters
+	//void setnsigma_e_TPC(Float_t f)                     { nsigma_e_TPC = f;         }
+    //void setnsigma_e_TOF(Float_t f)                     { nsigma_e_TOF = f;         }
+	//void setnsigma_pi_TPC(Float_t f)                     { nsigma_pi_TPC = f;         }
+	//void setnsigma_pi_TOF(Float_t f)                     { nsigma_pi_TOF = f;         }
+	//void setnsigma_K_TPC(Float_t f)                     { nsigma_K_TPC = f;         }
+	//void setnsigma_K_TOF(Float_t f)                     { nsigma_K_TOF = f;         }
+	//void setnsigma_p_TPC(Float_t f)                     { nsigma_p_TPC = f;         }
+	//void setnsigma_p_TOF(Float_t f)                     { nsigma_p_TOF = f;         }
+	//void setTRDSignal(Float_t f)                     { TRD_signal = f;         }
+	//void setTRDsumADC(Float_t f)                     { TRDsumADC = f;         }
+	//void setdca(Float_t f)                    { dca = f;        }
+	void set_TLV_part(TLorentzVector tlv)     { TLV_part = tlv; }
+    void settracknumber(Int_t number)           {track_number = number;}
+    void seteventnumber(Int_t number)               {event_no = number;}
+	//void setNTPCcls(UShort_t s)               { NTPCcls = s;}
+	//void setNTRDcls(UShort_t s)               { NTRDcls = s;}
+	//void setNITScls(UShort_t s)               { NITScls = s;}
+	//void setStatus(UShort_t s)                { status = s;}
+	//void setTPCchi2(Float_t f)                { TPCchi2 = f;}
+    //void setimpact_angle_on_TRD(Float_t f)           {impact_angle_on_TRD = f;}
+	void setTPCdEdx(Float_t f)                       {TPCdEdx = f;}
+	//void setTOFsignal(Float_t f)                     {TOFsignal = f;}
+    //void setTrack_length(Float_t f)                  {Track_length = f;}
+    void setmomentum(Float_t f)                     {momentum = f;}
+    void setenergy(Float_t f)                       {energy = f;}
+    void seteta(Float_t f)            {eta_track = f;}
+    void setpT(Float_t f)             {pT_track = f;}
+    void settheta(Float_t f)           {theta_track = f;}
+    void setphi(Float_t f)              {phi_track = f;}
+    void setsectorofincidence(Int_t f)    {sector_of_incidence = f;}
+    void setHelix(Float_t a, Float_t b,Float_t c,Float_t d,Float_t e,Float_t f)
+        {
+            aliHelix_params[0] = a;
+            aliHelix_params[1] = b;
+            aliHelix_params[2] = c;
+            aliHelix_params[3] = d;
+            aliHelix_params[4] = e;
+            aliHelix_params[5] = f;
+        }
+    /*
+    void setHelix_TRD(Float_t a, Float_t b,Float_t c,Float_t d,Float_t e,Float_t f)
+        {
+            aliHelix_TRD_params[0] = a;
+            aliHelix_TRD_params[1] = b;
+            aliHelix_TRD_params[2] = c;
+            aliHelix_TRD_params[3] = d;
+            aliHelix_TRD_params[4] = e;
+            aliHelix_TRD_params[5] = f;
+        }
+    */
+
+	// getters
+    /*
+    Float_t getnsigma_e_TPC() const                     { return nsigma_e_TPC;         }
+
+    Float_t getnsigma_e_TOF() const                     { return nsigma_e_TOF;         }
+	Float_t getnsigma_pi_TPC() const                     { return nsigma_pi_TPC;         }
+	Float_t getnsigma_pi_TOF() const                     { return nsigma_pi_TOF;         }
+	Float_t getnsigma_K_TPC() const                     { return nsigma_K_TPC;         }
+	Float_t getnsigma_K_TOF() const                     { return nsigma_K_TOF;         }
+	Float_t getnsigma_p_TPC() const                     { return nsigma_p_TPC;         }
+	Float_t getnsigma_p_TOF() const                     { return nsigma_p_TOF;         }
+	Float_t getTRDSignal() const                     { return TRD_signal;         }
+	Float_t getTRDsumADC() const                     { return TRDsumADC;         }
+	Float_t getdca() const                    { return dca;        }
+    */
+	TLorentzVector get_TLV_part() const       { return TLV_part;   }
+    Int_t gettracknumber() const                {return track_number;}
+    Int_t geteventnumber() const                {return event_no;}
+    /*
+	UShort_t getNTPCcls() const               { return NTPCcls;    }
+	UShort_t getNTRDcls() const               { return NTRDcls;    }
+	UShort_t getNITScls() const               { return NITScls;    }
+	UShort_t getStatus() const               { return status;    }
+	Float_t  getTPCchi2() const              { return TPCchi2; }
+    
+    Float_t   getimpact_angle_on_TRD() const    { return impact_angle_on_TRD; }
+    */
+	Float_t   getTPCdEdx() const                { return TPCdEdx; }
+	//Float_t   getTOFsignal() const              { return TOFsignal; }
+    //Float_t   getTrack_length() const           { return Track_length; }
+    Float_t   getHelix_param(Int_t i_param) const              {return aliHelix_params[i_param]; }
+    //Float_t   getHelix_TRD_param(Int_t i_param) const              {return aliHelix_TRD_params[i_param]; }
+    Float_t getmomentum() const                     {return momentum;}
+    Float_t getenergy() const                       {return energy;}
+    Float_t geteta() const                          {return eta_track;}
+    Float_t getpT() const               {return pT_track;}
+    Float_t getTheta() const            {return theta_track;}
+    Float_t getPhi() const              {return phi_track;}
+    Int_t getsectorofincidence() const  {return sector_of_incidence;}
+
+        
+
+
+    void Evaluate(Double_t t, // helix evaluation, taken from AliHelix
+                     Double_t r[3]);  //radius vector
+    
+    
+    // point to TRD_Tracklets as subclass
+
+    //---------------------------------------------------
+     Ali_TRD_ST_Tracklets_for_GNN* createTracklet() // online tracklet which is used for GNN input creation
+	{
+	    if (fNumTracklets == fTracklets->GetSize())
+		fTracklets->Expand( fNumTracklets + 10 );
+	    if (fNumTracklets >= 650000)
+	    {
+		Fatal( "Ali_TRD_ST_TPC_Track_GNN:: createTracklet()", "ERROR: Too many tracklets (>650000)!" );
+		exit( 2 );
+	    }
+
+	    new((*fTracklets)[fNumTracklets++]) Ali_TRD_ST_Tracklets_for_GNN;
+	    return (Ali_TRD_ST_Tracklets_for_GNN*)((*fTracklets)[fNumTracklets - 1]);
+	}
+	void clearTrackletList()
+	{
+	    fNumTracklets   = 0;
+	    fTracklets      ->Clear();
+	}
+	Int_t getNumTracklets() const
+	{
+	    return fNumTracklets;
+	}
+	Ali_TRD_ST_Tracklets_for_GNN* getTracklet(Int_t i) const
+	{
+	    return i < fNumTracklets ? (Ali_TRD_ST_Tracklets_for_GNN*)((*fTracklets)[i]) : NULL;
+        }
+        //----------------------------
+    ClassDef(Ali_TRD_ST_TPC_Track_GNN, 1);  // A simple track of a particle to save entries for GNN
+};
 
 
 //----------------------------------------------------------------------------------------
